44514ada0f18d22e2f746f5fe5fc044bf317e50c
nnhhaadd <nnhhaadd@gmail.com>
2020-12-17 12:29:48 +0700

    update auth procedures

--- SQL/procedures/auth.sql
+++ SQL/procedures/auth.sql
@@ -1,9 +1,12 @@
 use publication;
 
-ALTER TABLE scientist
-ADD HASHED_PASS TEXT NOT NULL;
+-- turn this alter table on if the table does not contain hashed_pass column
+-- ALTER TABLE scientist
+-- ADD or alter HASHED_PASS TEXT NOT NULL;
 
 drop procedure if exists get_user_roles;
+drop procedure if exists create_user;
+drop procedure if exists get_hashed_password;
 
 delimiter $$
 create procedure get_user_roles
@@ -21,6 +24,57 @@ begin
     
     select isAuthor, isContactAuthor, isReviewer, isEditor;
 end$$
+
+create procedure create_user
+(
+    user_id varchar(45),
+    fname text,
+    address text,
+    email text,
+    company text,
+    job text,
+    degree text,
+    profession text,
+    hashed_pass text,
+    work_email text,
+    is_author int,
+    is_contact_author int,
+    is_reviewer int,
+    is_editor int
+)
+begin
+    insert into scientist
+        (id, hashed_pass, fname, address, email, company, job, degree, profession) 
+        VALUES (user_id,hased_pass,fname,address,email,company,job,degree,profession);
+    
+    -- a scientist can not be an author/contact author and a editor at the same time --
+    -- this will be checked using a trigger
+
+    if (is_author) then
+        insert into author (s_id) values (user_id);
+	end if;
+    if (is_contact_author) then
+        insert into contact_author (s_id) values (user_id);
+	end if;
+    if (is_reviewer) then
+        insert into reviewer (s_id, work_email, collaboration_date) values (user_id, work_email, curdate());
+	end if;
+    if (is_editor) then
+        insert into editor (s_id, appointed_date) values ('test', curdate());	
+    end if;
+end$$
+
+create procedure get_hashed_password
+(
+    user_id varchar(45)
+)
+begin
+    select id, hased_pass from scientist
+    where id = user_id;
+end$$
+
 delimiter ;
 
 grant EXECUTE on procedure publication.get_user_roles to authentication@localhost;
+grant execute on procedure publication.create_user to authentication@localhost;
+grant execute on procedure publication.get_hashed_password to authentication@localhost;
---
+++ SQL/triggers.sql
@@ -0,0 +1,28 @@
+use publication;
+
+drop trigger if exists author_editor;
+
+delimiter $$
+create trigger author_editor
+after insert
+on editor
+for each row
+begin
+    declare found_user_as_author int;
+    declare found_user_as_contact_author int;
+
+    set found_user_as_author = (select count(s_id)
+    from author
+    where s_id = new.s_id);
+
+    set found_user_as_contact_author = (
+		select count(s_id)
+		from contact_author
+		where s_id = new.s_id
+    );
+
+    if(found_user_as_author > 0 or found_user_as_contact_author > 0) then
+        signal sqlstate '45000' set message_text = 'Author/Editor policy is violated.';
+	end if;
+end;$$
+delimiter ;
